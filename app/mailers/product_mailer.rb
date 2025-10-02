c5b46ae3627a8bfa17b4fc3b50fb637503ae9b7c
RafaSSii <rafarsk78@gmail.com>
2025-10-02 19:43:37 -0300

    Adding In Stock Notifications | In Stock Email Notifications

---
+++ app/mailers/product_mailer.rb
@@ -0,0 +1,11 @@
+class ProductMailer < ApplicationMailer
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.product_mailer.in_stock.subject
+  #
+  def in_stock
+    @product = param[:product]
+    mail to: param[:subscriber].mail
+  end
+end
